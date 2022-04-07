b513ec6b0d869ce0f0616cb415606627f48b474b
Johannes Messner <messnerjo@gmail.com>
2022-04-07 15:29:59 +0200

    feat: add infix operators

--- pyskell/__init__.py
+++ pyskell/__init__.py
@@ -1,3 +1,9 @@
+def partial(fn, *args):
+    def partial_fn(*inner_args):
+        return fn(*args, *inner_args)
+    return partial_fn
+
+
 def curry_(fn, known_args, new_args):
     num_args = fn.__code__.co_argcount
     if len(known_args) + len(new_args) == num_args:
@@ -11,12 +17,18 @@ def curry(fn):
         return curry_(fn, known_args=(), new_args=args)
     return curried
 
-
-class partial():
+# credit: http://tomerfiliba.com/blog/Infix-Operators/
+class Infix(object):
     def __init__(self, fn):
         self.fn = fn
+    def __or__(self, other):
+        return self.fn(other)
+    def __ror__(self, other):
+        return Infix(partial(self.fn, other))
+    def __call__(self, v1, v2):
+        return self.fn(v1, v2)
 
-    def __call__(self, *args, **kwargs):
-        def _partial_fn(*inner_args):
-            return self.fn(*args, *inner_args)
-        return _partial_fn
+
+@Infix
+def infix_add(x, y):
+    return x + y
--- pyskell/pyskell.py
+++
@@ -1,4 +0,0 @@
-def partial(fn, arg):
-    def partial_fn(*args):
-        return fn(arg, *args)
-    return partial_fn
